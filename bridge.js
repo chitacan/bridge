6d754559579000d4838f5f665870436aba8a6a81
kyung yeol kim <chitacan@gmail.com>
2014-09-06 19:06:44 +0900

    use namespace api to reference other namespace.

--- bridge.js
+++ bridge.js
@@ -5,16 +5,22 @@ var _       = require('lodash')
   , debug_c = debug('server:client')
   , debug_d = debug('server:deamon');
 
+var DAEMON_NSP = '/bridge/deamon'
+  , CLIENT_NSP = '/bridge/client';
+
 function Bridge(io) {
   if (!(this instanceof Bridge)) {
     return new Bridge(io);
   }
+  this.io = io;
 
-  this.client = io.of('/bridge/client');
-  this.daemon = io.of('/bridge/daemon');
+  this.client = io.of(DAEMON_NSP);
+  this.daemon = io.of(CLIENT_NSP);
 
   this.client.on('connection', onClientConnect);
   this.daemon.on('connection', onDaemonConnect);
+
+  return this;
 }
 
 function onClientConnect(socket) {
@@ -23,6 +29,7 @@ function onClientConnect(socket) {
     return;
   }
 
+  var daemon = this.server.of(DAEMON_NSP);
   debug_c('connected : ' + socket.id);
   socket.on('data', function(data) {
     daemon.emit('data', data);
@@ -38,6 +45,7 @@ function onDaemonConnect(socket) {
     return;
   }
 
+  var client = this.server.of(CLIENT_NSP);
   debug_d('connected : ' + socket.id);
   socket.on('res', function(data) {
     if (data.binary) {
