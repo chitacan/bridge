49a64601b8a84edd48df4994c994579c010f05d2
kyung yeol kim <chitacan@gmail.com>
2014-09-08 00:54:34 +0900

    use command module to parse & execute.
     - add comamnd module.
     - now we can specify url & port number via commandline.

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
-var net   = require('net');
-var debug = require('debug');
-var debug_a = debug('client:adb');
-var debug_c = debug('client:bridge');
+module.exports = Client;
 
-var server = net.createServer(function(c) {
-  server.socket = c;
-  debug_a('connected');
-  c.on('close', function() {
-    debug_a('disconnected');
+var net   = require('net')
+  , u     = require('url')
+  , debug = require('debug')
+  , debug_a = debug('client:adb')
+  , debug_c = debug('client:bridge');
+
+function Client(opt) {
+  if (!(this instanceof Client)) {
+    return new Client(opt);
+  }
+
+  var port = opt.port;
+  var url  = u.resolve(opt.url, 'bridge/client');
+
+  this.server = createServer(port);
+  this.socket = createSocket(url);
+
+  return this;
+}
+
+function createServer(port) {
+  var server = net.createServer(function(c) {
+    server.socket = c;
+    debug_a('connected');
+    c.on('close', function() {
+      debug_a('disconnected');
+    });
+    c.on('data', function(chunk) {
+      if (!!socket && socket.connected) {
+        socket.emit('data', { binary: chunk });
+      }
+    });
+    c.on('error', function(e) {
+      debug_a(e);
+    });
   });
-  c.on('data', function(chunk) {
-    if (!!socket && socket.connected) {
-      socket.emit('data', { binary: chunk });
+  server.maxConnection = 1;
+  server.listen(port, function() {
+    this.port = this.address().port;
+    debug_a('Socket server listening on port ' + this.port);
+  });
+
+  return server;
+}
+
+function createSocket(url) {
+  debug_c('connecting to : ' + url);
+  var socket = require('socket.io-client')(url);
+
+  socket.on('connect', function(){
+    debug_c('connected');
+  });
+  socket.on('data', function(data){
+    debug_c('client data');
+  });
+  socket.on('disconnect', function(){
+    debug_c('disconnected');
+  });
+  socket.on('res', function(data) {
+    if (data.binary) {
+      server.socket.write(data.binary);
     }
   });
-  c.on('error', function(e) {
-    debug_a(e);
+  socket.on('error', function(e) {
+    debug_c(e);
   });
-});
-server.maxConnection = 1;
-server.listen(0, function() {
-  this.port = this.address().port;
-  debug_a('Socket server listening on port ' + this.port);
-});
 
-var URL    = 'http://chitacan.redribbon.io/bridge/client';
-var socket = require('socket.io-client')(URL);
-
-socket.on('connect', function(){
-  debug_c('connected');
-});
-socket.on('data', function(data){
-  debug_c('client data');
-});
-socket.on('disconnect', function(){
-  debug_c('disconnected');
-});
-socket.on('res', function(data) {
-  if (data.binary) {
-    server.socket.write(data.binary);
-  }
-});
-socket.on('error', function(e) {
-  debug_c(e);
-});
+  return socket;
+}
