4d49cae9bf26b9088c91b6dbc9fdfe14724a08ad
kyung yeol kim <chitacan@gmail.com>
2014-09-17 20:09:26 +0900

    [server] implement bridge status page.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,20 @@
-var app = angular.module('bridge', ['ngResource'])
+var app = angular.module('bridge-manager', ['ngResource'])
+
+app.controller('statusCtrl', ['$scope', 'bridge', function($scope, bridge) {
+  bridge.query().$promise.then(function(bridges) {
+    console.log(bridges);
+  });
+}]);
+
+app.factory('bridge', function($resource) {
+  return $resource(
+    '/api/bridge',
+    {},
+    {
+      'query'  : { method: 'GET', isArray: true },
+      'get'    : { method: 'GET' },
+      'create' : { method: 'PUT' },
+      'remove' : { method: 'DELETE' }
+    }
+  )
+});
