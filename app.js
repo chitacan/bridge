49230f986f1ea1e60fa74b83528b821baceecb20
kyung yeol kim <chitacan@gmail.com>
2014-09-23 16:46:30 +0900

    [server] change app.js structure.

--- app.js
+++ app.js
@@ -5,60 +5,64 @@ var express      = require('express')
   , cookieParser = require('cookie-parser')
   , bodyParser   = require('body-parser')
 
-var app    = express()
-  , server = http.Server(app)
-  , io     = require('socket.io').listen(server)
-  , bridge = require('./bridge')(io);
+module.exports = create;
 
-var routes = require('./routes/index')
-var api    = require('./routes/api')(bridge)
+function create(dep) {
+  var app    = express()
+    , server = http.Server(app)
+    , io     = require('socket.io').listen(server)
+    , bridge = dep || require('./bridge');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  var routes = require('./routes/index')
+  var api    = require('./routes/api')(bridge(io))
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use('/static', express.static(path.join(__dirname, 'public')));
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use('/'    , routes);
-app.use('/api' , api);
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use('/static', express.static(path.join(__dirname, 'public')));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+  app.use('/'    , routes);
+  app.use('/api' , api);
+
+  /// catch 404 and forward to error handler
+  app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+  });
 
-/// error handlers
+  /// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  require('longjohn');
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    require('longjohn');
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
     });
-}
+  }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: {}
     });
-});
+  });
 
-module.exports = {
-  app : app,
-  server: server
+  return {
+    app : app,
+    server: server
+  }
 }
